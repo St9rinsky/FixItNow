56aa127e4a66ac80e73506501161188c90688958
sello letswalo <seletjhb025@student.wethinkcode.co.za>
2026-09-15 18:30:20 +0200

    feat: added getters and setters for mentainance

--- src/main/java/MaintenanceRequest.java
+++ src/main/java/MaintenanceRequest.java
@@ -5,12 +5,54 @@ public class MaintenanceRequest {
     private String description;
 
     private Unit unit;
+    private User createdBy;
 
-    public MaintenanceRequest(int id, String title, String description, Unit unit) {
+    public MaintenanceRequest(int id, String title, String description, Unit unit, User user) {
         this.id = id;
         this.title = title;
         this.description = description;
 
         this.unit = unit;
+        this.createdBy = user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Unit getUnit() {
+        return unit;
+    }
+
+    public void setUnit(Unit unit) {
+        this.unit = unit;
+    }
+
+    public User getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(User createdBy) {
+        this.createdBy = createdBy;
     }
 }
