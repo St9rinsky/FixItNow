5dd583b4ed352b53b64ebf3af975f2f29b7e0b58
sello letswalo <seletjhb025@student.wethinkcode.co.za>
2026-09-16 23:50:16 +0200

    fead: added mantiance request list
    refac: sorted the code into sections

--- src/main/java/Domain/Unit.java
+++ src/main/java/Domain/Unit.java
@@ -1,16 +1,22 @@
 package Domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Unit {
 
-    private int id;
-    private String unitNumber;
+    private final int id;
+    private final String unitNumber;
     private Property property;
+    private final List<MaintenanceRequest> maintenanceRequests;
 
     public Unit(int id, String unitNumber) {
         this.id = id;
         this.unitNumber = unitNumber;
+        this.maintenanceRequests = new ArrayList<>();
     }
 
+    //-----------------GETTERS------------------//
     public int getId() {
         return id;
     }
@@ -23,7 +29,22 @@ public class Unit {
         return property;
     }
 
+    public List getMaintenanceRequests() {
+        return this.maintenanceRequests;
+    }
+
+    //---------------------SETTERS-------------------//
+
     public void setProperty(Property property) {
         this.property = property;
     }
+
+    //------------------------------------------------------------------------------------------//
+
+    public void addMaintenanceRequest (MaintenanceRequest request) {
+        this.maintenanceRequests.add(request);
+        request.setUnit(this);
+    }
+
+
 }
