684241e0f6efc7d0656d518e69b3e2c41ba148e2
sello letswalo <seletjhb025@student.wethinkcode.co.za>
2026-09-17 00:06:57 +0200

    refac: sorted code into sections

--- src/main/java/Domain/MaintenanceRequest.java
+++ src/main/java/Domain/MaintenanceRequest.java
@@ -2,59 +2,83 @@ package Domain;
 
 public class MaintenanceRequest {
 
-    private int id;
-    private String title;
+    private final int id;
+    private final String title;
     private String description;
+    private final String createdAt;
+    private String updatedAt;
 
     private Unit unit;
-    private User createdBy;
+    private final User createdBy;
 
-    public MaintenanceRequest(int id, String title, String description, Unit unit, User user) {
+    private Status status;
+    private Priority priority;
+
+    public MaintenanceRequest(int id, String title, String description, User user) {
         this.id = id;
         this.title = title;
         this.description = description;
 
-        this.unit = unit;
         this.createdBy = user;
+        this.createdAt = "now";
     }
 
+    //----------------------------------------GETTERS---------------------------------//
     public int getId() {
         return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getDescription() {
         return description;
+    }
+
+
+    public Unit getUnit() {
+        return unit;
+    }
+
+    public User getAuthor() {
+        return createdBy;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+
+    //------------------------------------------SETTERS------------------------------------//
+    public void updateDescription(String description) {
+        this.description = description;
+    }
+
+    public void updateTime(String updated) {
+        this.updatedAt = updated;
+    }
+    public void setUnit(Unit unit) {
+        this.unit = unit;
     }
 
     public void setDescription(String description) {
         this.description = description;
     }
 
-    public Unit getUnit() {
-        return unit;
+    public void updateStatus(Status status) {
+        this.status = status;
     }
 
-    public void setUnit(Unit unit) {
-        this.unit = unit;
-    }
-
-    public User getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(User createdBy) {
-        this.createdBy = createdBy;
+    public void updatePriority(Priority priority) {
+        this.priority = priority;
     }
 }
