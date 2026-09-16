f54c56fe3d893de57b48b431fa42575d077f8a58
sello letswalo <seletjhb025@student.wethinkcode.co.za>
2026-09-17 00:30:25 +0200

    feat: service layer now creates requests

--- src/main/java/Service/MaintenanceRequestService.java
+++ src/main/java/Service/MaintenanceRequestService.java
@@ -1,4 +1,19 @@
 package Service;
 
+import Domain.MaintenanceRequest;
+import Domain.Unit;
+import Domain.User;
+
 public class MaintenanceRequestService {
+
+    public MaintenanceRequest createRequest(
+            String title,
+            String description,
+            User user,
+            Unit unit) {
+        MaintenanceRequest request = new MaintenanceRequest(title, description, user);
+        unit.addMaintenanceRequest(request);
+
+        return request;
+    }
 }
