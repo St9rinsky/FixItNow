8ffdfe47e415790a81db89eafb6b94648d85d793
sello letswalo <seletjhb025@student.wethinkcode.co.za>
2026-09-17 03:19:59 +0200

    the service is now a bean

--- src/main/java/com/fixitnow/Service/MaintenanceRequestService.java
+++ src/main/java/com/fixitnow/Service/MaintenanceRequestService.java
@@ -4,7 +4,9 @@ import com.fixitnow.Domain.MaintenanceRequest;
 import com.fixitnow.Domain.Unit;
 import com.fixitnow.Domain.User;
 import com.fixitnow.Repository.MaintenanceRequestRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MaintenanceRequestService {
     private final MaintenanceRequestRepository repo;
 
