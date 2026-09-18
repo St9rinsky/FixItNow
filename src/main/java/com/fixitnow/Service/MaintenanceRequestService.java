8dbbaa05960a3680036e7ef34c5fc38aafcae2dd
sello letswalo <seletjhb025@student.wethinkcode.co.za>
2026-09-18 19:20:29 +0200

    feat: constructor now initialises, user and unit repo
    patch: create request now takes user id and unit id to find existing users and ids, instead of finding by whole entity

--- src/main/java/com/fixitnow/Service/MaintenanceRequestService.java
+++ src/main/java/com/fixitnow/Service/MaintenanceRequestService.java
@@ -4,21 +4,32 @@ import com.fixitnow.Domain.MaintenanceRequest;
 import com.fixitnow.Domain.Unit;
 import com.fixitnow.Domain.User;
 import com.fixitnow.Repository.MaintenanceRequestRepository;
+import com.fixitnow.Repository.UnitRepository;
+import com.fixitnow.Repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MaintenanceRequestService {
     private final MaintenanceRequestRepository repo;
+    private final UserRepository userRepo;
+    private final UnitRepository unitRepo;
 
-    public MaintenanceRequestService(MaintenanceRequestRepository repository) {
+    public MaintenanceRequestService(
+            MaintenanceRequestRepository repository,
+            UserRepository userRepo,
+            UnitRepository unitRepo) {
         this.repo = repository;
+        this.userRepo = userRepo;
+        this.unitRepo = unitRepo;
     }
 
     public MaintenanceRequest createRequest(
             String title,
             String description,
-            User user,
-            Unit unit) {
+            Long userId,
+            Long unitId) {
+        User user = userRepo.findById(userId).orElseThrow();
+        Unit unit = unitRepo.findById(unitId).orElseThrow();
         MaintenanceRequest request = new MaintenanceRequest(title, description, user);
         unit.addMaintenanceRequest(request);
 
