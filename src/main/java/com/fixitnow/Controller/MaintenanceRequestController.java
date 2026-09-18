2614afb5d0d349f35a2ba88594e0dee2a9f8e6b5
sello letswalo <seletjhb025@student.wethinkcode.co.za>
2026-09-18 19:36:08 +0200

    feat: implemented controller class, rough-work hardcoding some unit and user objects for now

--- src/main/java/com/fixitnow/Controller/MaintenanceRequestController.java
+++ src/main/java/com/fixitnow/Controller/MaintenanceRequestController.java
@@ -1,4 +1,41 @@
 package com.fixitnow.Controller;
 
+import com.fixitnow.Domain.MaintenanceRequest;
+import com.fixitnow.Domain.Roles;
+import com.fixitnow.Domain.Unit;
+import com.fixitnow.Domain.User;
+import com.fixitnow.Service.MaintenanceRequestService;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/maintenance-requests")
 public class MaintenanceRequestController {
+    private final MaintenanceRequestService service;
+
+    public MaintenanceRequestController(MaintenanceRequestService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public MaintenanceRequest createRequest() {
+
+        User user = new User(
+                "sello",
+                "sello@gmail.com",
+                "+1234",
+                Roles.TENANT
+        );
+
+        Unit unit = new Unit("210");
+
+        return service.createRequest(
+                "Leaking tap",
+                "The kitchen tap is leaking.",
+                1L,
+                1L
+        );
+    }
 }
