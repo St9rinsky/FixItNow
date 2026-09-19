e537001897861c5e14caf646f8ac475f35845321
sello letswalo <seletjhb025@student.wethinkcode.co.za>
2026-09-19 15:45:23 +0200

    feat: exception handle for not found resources and its DTO responce

--- src/main/java/com/fixitnow/DTO/Response/ErrorResponse.java
+++ src/main/java/com/fixitnow/DTO/Response/ErrorResponse.java
@@ -1,4 +1,19 @@
 package com.fixitnow.DTO.Response;
 
 public class ErrorResponse {
+    private int status;
+    private String message;
+
+    public ErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
--- src/main/java/com/fixitnow/Exceptions/ExceptionsHandler.java
+++ src/main/java/com/fixitnow/Exceptions/ExceptionsHandler.java
@@ -1,16 +1,20 @@
 package com.fixitnow.Exceptions;
 
+import com.fixitnow.DTO.Response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class ExceptionsHandler {
 
-    @ExceptionHandler(ResourceNotFoundException.class);
-    @ResponseStatus(HttpStatus.NOT_FOUND);
-    public String handleResourceNotFound(ResourceNotFoundException ex) {
-        return ex.getMessage();
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleResourceNotFound(ResourceNotFoundException exception) {
+
+        ErrorResponse response = new ErrorResponse(404, exception.getMessage());
+
+        return response;
     }
 }
