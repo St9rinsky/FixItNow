7c8d889f595e4e22eef11672b82e9561c01a093a
sello letswalo <seletjhb025@student.wethinkcode.co.za>
2026-09-19 18:53:16 +0200

    feat: added before each for tests and tests for update priority

--- src/test/java/com/fixitnow/Domain/MaintenanceRequestTest.java
+++ src/test/java/com/fixitnow/Domain/MaintenanceRequestTest.java
@@ -1,21 +1,33 @@
 package com.fixitnow.Domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MaintenanceRequestTest {
+    private MaintenanceRequest request;
+
+    @BeforeEach
+    void setUp(){
+        request = new MaintenanceRequest(
+                "broken window",
+                "bedroom window wont close",
+                null
+        );
+    }
+
+
     @Test
     void shouldUpdateStatus() {
-
-        MaintenanceRequest request =
-                new MaintenanceRequest(
-                        "broken window",
-                        "bedroom window won't close.",
-                        null
-                );
         assertEquals(Status.OPEN, request.getStatus());
         request.updateStatus(Status.IN_PROGRESS);
         assertEquals(Status.IN_PROGRESS, request.getStatus());
+    }
+
+    @Test
+    void shouldUpdatePriority() {
+        request.updatePriority(Priority.HIGH);
+        assertEquals(Priority.HIGH, request.getPriority());
     }
 }
